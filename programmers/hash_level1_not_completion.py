c293819bd70138774b22f3370a75f276c747e753
[hwon-n] <woo_oas@naver.com>
2022-02-06 23:31:52 +0900

    hash

--- programmers/hash_level1_not_completion.py
+++ programmers/hash_level1_not_completion.py
@@ -3,13 +3,16 @@ def solution(participant, completion):
     hash_dict = {}
     sum_hash = 0
     
+    # 해쉬 테이블 생성 + 총 합 계산
     for name in participant:
         hash_dict[hash(name)] = name
         sum_hash += hash(name)
         
+    # 완주 선수 삭제
     for name in completion:
         sum_hash -= hash(name)
-        
+    
+    # 총 합에서 완주된 선수를 삭제하고 나면 완주하지 못한 선수가 남음
     answer = hash_dict[sum_hash]
     
     return answer

9702a6553ce34379e05110a3e616f56ed15e94b7
[hwon-n] <woo_oas@naver.com>
2022-07-17 01:50:49 +0900

    skill check level 1

---
+++ programmers/what_day.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def solution(a, b):
+
+    dateDict = {
+        0: 'MON',
+        1: 'TUE',
+        2: 'WED',
+        3: 'THU',
+        4: 'FRI',
+        5: 'SAT',
+        6: 'SUN'
+    }
+    
+    time = datetime.strptime(f'2016-{a}-{b}', '%Y-%m-%d').weekday()
+    
+    return dateDict[time]
